2d8532fbba0e5cd946bf2d62603acc5a1fdfa0fa
S0ph1ia <badalovasophia@gmail.com>
2021-02-28 19:39:30 +0000

    Added git ignored, added factorial code in NodeJs

--- index.js
+++ index.js
@@ -5,6 +5,31 @@
  * @param {!express:Response} res HTTP response context.
  */
 exports.helloWorld = (req, res) => {
-    let message = req.query.message || req.body.message || 'Hello World!';
-    res.status(200).send(message);
-  };
+  let message = req.query.message || req.body.message || "0";
+  benchmarking(message);
+  res.status(200).send("Factorial Run Successfully");
+};
+
+function factorial(num) {
+  // checking if number is negative
+  if (number < 0) {
+    console.log("Error! Factorial for negative number does not exist.");
+  }
+
+  // if number is 0
+  else if (number === 0) {
+    console.log(`The factorial of ${number} is 1.`);
+  }
+
+  // if number is positive
+  else {
+    let fact = 1;
+    for (i = 1; i <= number; i++) {
+      fact *= i;
+    }
+  }
+}
+
+function benchmarking(num){
+    factorial(num);
+}
